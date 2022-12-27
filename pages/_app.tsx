fbf419edfb988309864b08fc3e6d689072773940
Timetravelapes <102059377+Timetravelapes@users.noreply.github.com>
2022-12-27 16:37:34 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>Timetravelapes dApp</title>
+        <title>Timetravelapes NFT Mint Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
